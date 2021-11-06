60e6962df8c5607d9b62d5733966db36e5a2b854
Asurov <73227551+IAsurovI@users.noreply.github.com>
2021-11-06 22:16:57 +0200

    Add files via upload

---
+++ lab17.9._2(3)/lab3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            double fx = 0;
+            double b = 0;
+            int rbu = 0;
+            if (radioButton1.Checked) { fx = Math.Sinh(x); rbu = 1; }
+            else if (radioButton2.Checked) { fx = Math.Pow(x, 2); rbu = 2; }
+            else if (radioButton3.Checked) { fx = Math.Exp(x); rbu = 3; } 
+            
+            if (x / y > 0) b = Math.Log(fx) + Math.Pow((Math.Pow(fx, 2) + y), 3);
+            else if (x / y < 0) b = Math.Log(Math.Abs(fx) / y) + Math.Pow((fx + y), 3);
+            else if (x == 0) b = Math.Pow((Math.Pow(fx, 2) + y), 3);
+            else if (y == 0) b = 0;
+
+            textBox3.Text += Environment.NewLine + "Результаты работы программы:";
+            textBox3.Text += Environment.NewLine + "Выбран: radioButton " + rbu;
+            textBox3.Text += Environment.NewLine + "При Х= " + x;
+            textBox3.Text += Environment.NewLine + "При Y= " + y;
+            textBox3.Text += Environment.NewLine + "b= " + b;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
---
+++ lab7_9._1(2)/lab17_9._1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab17_9._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text += Environment.NewLine + "Лаб. раб. N1. Марков";
+            double x = double.Parse(textBox1.Text);
+            textBox4.Text += Environment.NewLine + "X= " + x.ToString();
+            double y = double.Parse(textBox2.Text);
+            textBox4.Text += Environment.NewLine + "Y= " + y.ToString();
+            double z = double.Parse(textBox3.Text);
+            textBox4.Text += Environment.NewLine + "Z= " + z.ToString();
+            //double a1 = Math.Pow((8 + Math.Pow(Math.Abs(x - y), 2) + 1), 1 / 3);
+            //double a2= Math.Pow(x,2)+Math.Pow(y,2)+2;
+            //double a3= Math.Exp(Math.Abs(x-y));
+            //double a4=Math.Pow(((Math.Pow(Math.Tan(z),2))+1),x);
+            //double u = (a1 / a2) - (a3 * a4);
+            double a1 = Math.Pow((8 + Math.Pow(Math.Abs(x - y), 2) + 1), 1 / 3);
+            double a2 = Math.Pow(x, 2) + Math.Pow(y, 2) + 2;
+
+            double a3 = Math.Exp(Math.Abs(x - y));
+            double a4 = Math.Pow(((Math.Pow(Math.Tan(z), 2)) + 1), x);
+
+            double u = (a1 / a2) - (a3 * a4);
+
+            textBox4.Text += Environment.NewLine + "U= " + u.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double x = -4.5;
+            double y = 0.75 * Math.Pow(10, -4);
+            double z = 0.845 * Math.Pow(10, 2);
+            textBox1.Text += Environment.NewLine + x.ToString();
+            textBox2.Text += Environment.NewLine + y.ToString();
+            textBox3.Text += Environment.NewLine + z.ToString();
+        }
+    }
+}
