6f7bfd6332b706b88eb438f2595acde65fa111e0
Asurov <73227551+IAsurovI@users.noreply.github.com>
2021-11-06 22:18:21 +0200

    Add files via upload

---
+++ lab6p1/lab6p1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab6p1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public  void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int[][] A;
+            int[][] B;
+            int n;
+            n = Convert.ToInt32(textBox1.Text);
+            A = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = new int[n];
+                for (int j = 0; j < n; j++) A[i][j] = rand.Next(n) + 1;
+
+                if (A[i][0] > A[i][n - 1])
+                {
+                    int a = A[i][0];
+                    A[i][0] = A[i][n - 1];
+                    A[i][n - 1] = a;
+                }
+            }
+            Data1(A, n);
+            B = new int[n][];
+            int kolSt;
+            for (int i = 0; i < n; i++)
+            {
+                kolSt = A[i][n - 1] - A[i][0] + 1;
+                B[i] = new int[kolSt];
+                for (int j = 0; j < kolSt; j++) B[i][j] = A[i][A[i][0] - 1 + j];
+            }
+            dataGridView2.ColumnCount = n;
+            dataGridView2.RowCount = n;
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B[i].Length; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = B[i][j];
+                    dataGridView2.Columns[i].Width = 20;
+                    dataGridView2.Columns[i].Name = $"B{i}";
+                }
+                }
+            for (int i = 0; i < n; i++) Array.Sort(A[i]);
+            Data3(A, n);
+        }
+        public void Data1(int[][] A, int n)
+        {
+            dataGridView1.ColumnCount = n;
+            dataGridView1.RowCount = n;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = A[i][j];
+                    dataGridView1.Columns[i].Width = 20;
+                    dataGridView1.Columns[i].Name = $"A{i}";
+                }           
+            }
+        }
+        public void Data3(int[][] A, int n)
+        {
+            dataGridView3.ColumnCount = n;
+            dataGridView3.RowCount = n;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dataGridView3.Rows[i].Cells[j].Value = A[i][j];
+                    dataGridView3.Columns[i].Width = 20;
+                    dataGridView3.Columns[i].Name = $"A{i}";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "9";
+            label5.Text = "Завдання №3: Кожен рядок матриці A на першій і останній позиції містить індекси відповідно початку і кінця діапазону елементів рядка,\n які необхідно переписати у відповідний рядок матриці B. Створити матрицю B з необхідною кількістю стовпчиків у кожному рядку та переписати\n до неї вказані елементи з матриці A. Відсортувати кожен рядок матриці A за зростанням ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+        public void clear()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView3.Columns.Clear();
+        }
+    }
+}
---
+++ lab9p1/lab9p1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lab9p1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//2.	Заданий рядок, що містить декілька слів,
+         //розділених пробілами. Необхідно надрукувати усі слова, що розпочинаються
+         //з гласної букви.
+            textBox2.Clear();
+            string input = textBox1.Text;
+            const string filter = "aeioyuAEIOYU";
+            string[] wordsOf = input.Split(' ');
+            for (int i = 0; i < wordsOf.Length; i++)
+            {
+                foreach (var filt in filter)
+                {
+                    if (wordsOf[i][0] == filt)
+                    {
+                        textBox2.Text+=wordsOf[i]+" ";
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Завдання: \n"+
+                "2.	Заданий рядок, що містить декілька слів, розділених пробілами.\n" +
+                " Необхідно надрукувати усі слова, що розпочинаються з гласної букви.";
+            label7.Text = "2.	В якому з них більше слів, що містять цифри?";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] mass = new int[3];
+            string str1 = richTextBox1.Lines[0];
+            string str2 = richTextBox1.Lines[1];
+            string str3 = richTextBox1.Lines[2];
+            foreach (string i in str1.Split(new char[] { ' ' })) mass[0] = prohod(i);
+            foreach (string i in str2.Split(new char[] { ' ' })) mass[1] = prohod(i);
+            foreach (string i in str3.Split(new char[] { ' ' })) mass[2] = prohod(i);
+            textBox3.Text = Convert.ToString(Array.IndexOf(mass, mass.Max()) + 1) + " рядок";
+        }
+        public int prohod(string i)
+        {
+            const string filter = "0123456789";
+            int output = 0;
+            foreach (var filt in filter)
+            {
+                if (i.Contains(filt))
+                {
+                    output++;
+                    break;
+                }
+            }
+            return output;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Lorem Ip2sum i3s sim4ply dummy text" +
+                "\nI3t has surv2ived nost on34ly fidve ce1nturies" +
+                "\nAl3dus PageMak1er inc2luding versions of Lorem Ipsum";
+        }
+    }
+}
