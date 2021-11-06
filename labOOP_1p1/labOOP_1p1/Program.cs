6d373b0241dcc8e55b88795a871e35decf7abdf3
Asurov <73227551+IAsurovI@users.noreply.github.com>
2021-11-06 22:19:09 +0200

    Add files via upload

---
+++ labOOP_1p1/labOOP_1p1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace labOOP_1p1
+{
+    class vector
+    {
+        public double x;
+        public double y;
+
+        public int n;
+        public double []vec;
+        public vector(double x, double y, int n)
+        {
+            this.vec = new double[n];
+
+            this.x = x;
+            this.y = y;
+            this.n = n;
+        }
+        public void inputNumber()
+        {
+            Console.WriteLine("\nВвод чисел: ");
+            for (int i = 0; i < this.n; i++)
+            {
+                vec[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        public void outputNumber()
+        {
+            Console.WriteLine("\nВывод чисел: ");
+            for (int i = 0; i < this.n; i++)
+            {
+                Console.WriteLine($"i[{i}]={vec[i]}");
+            }
+        }
+        public void lengthOf()
+        {
+            double length = 0;
+            double lengthDone = 0;
+            for (int i = 0; i < this.n; i++) 
+            {
+                length +=vec[i]*vec[i];
+                    }
+
+            lengthDone = Math.Sqrt(length);
+            Console.WriteLine($"\nДлина вектора: {lengthDone}");
+        }
+        public void normOf()
+        {
+            Console.WriteLine("\nНормированный вектор:");
+            double length = 0;
+            double lengthDone = 0;
+            for (int i = 0; i < this.n; i++)
+            {
+                length += vec[i] * vec[i];
+            }
+
+            lengthDone = Math.Sqrt(length);
+            for(int i = 0; i < this.n; i++)
+            {
+                Console.WriteLine($"i[{i}]={vec[i] / lengthDone}");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("х:");
+            double x= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Размерность вектора:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            vector vec = new vector(x, y, n);
+
+            vec.inputNumber();
+            vec.outputNumber();
+            vec.lengthOf();
+            vec.normOf();
+
+        }
+    }
+}
---
+++ labRegp1/labRegp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labRegp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox2.Text = Regex.Replace(textBox1.Text, @"\W+", "");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Задание: \n2. Дана строка вида «dafd#$245243564@#243$yet12&». \nНаписать программу, которая выводит на экран строку из цифр и латинских букв.";
+            textBox1.Text = "dafd#$245243564@#243$yet12&";
+
+        }
+    }
+    }
