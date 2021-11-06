bde96dd6bb62d1fca037b1ba4f7daa9f458750ed
Asurov <73227551+IAsurovI@users.noreply.github.com>
2021-11-06 22:16:03 +0200

    Add files via upload

---
+++ lab4p1/lab4p1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4p1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = Convert.ToDouble(textBox6.Text);
+            double x = x0;
+            textBox5.Text = "Работу выполнил ст. Марков Е." + Environment.NewLine;
+            while (x <= (xk + dx / 2))
+            {
+                double y = Math.Pow(10, -1) * a * Math.Pow(x, 3) * Math.Tan(a - b * x);
+                textBox5.Text += "x=" + Convert.ToString(x)  + "y=" + Convert.ToString(y) + Environment.NewLine;
+                x += dx;
+            }
+
+           
+        }
+
+        private void Form1_Load(object senderx, EventArgs e)
+        {
+            textBox1.Text = "-0,5";
+            textBox2.Text = "2,5";
+            textBox3.Text = "0,05";
+            textBox4.Text = "10,2";
+            textBox6.Text = "1,25";
+
+        }
+    }
+}
---
+++ lab5p1/lab5p1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5p1
+{
+    
+    public partial class Form1 : Form
+    {
+        int[] Mas = new int [15];
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {listBox1.Items.Clear();
+            for (int i = 0; i < 15; i++) 
+            {
+
+                //Mas[i] = rand.NextDouble()*50;
+                Mas[i] = rand.Next(-50, 50);
+                listBox1.Items.Add("Mas[" + i.ToString() + "]=" + Mas[i].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();             
+                int max = Mas[0];
+                int maxIndex = 0;
+                for (int i = 1; i < 15; i++)
+                {
+                    if (Mas[i] > max)
+                    {
+                        maxIndex = i;
+                        max = Mas[i];
+                    }
+                }
+                //listBox2.Items.Add(maxIndex);
+                int temp = Mas[14];
+                Mas[14] = Mas[maxIndex];
+                Mas[maxIndex ]= temp;
+            for(int i=0;i<15;i++)listBox2.Items.Add("Mas[" + i.ToString() + "]=" + Mas[i].ToString());
+            
+                
+
+            }
+        }
+        }
+      
+    
